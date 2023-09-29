aea5d3695d1d289a09c1d2990a1616753465feed
progmamun <mamun333089@gmail.com>
2023-09-29 22:39:50 +0600

    update-auth-response

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
 import httpStatus from "http-status";
 import catchAsync from "../../../shared/catchAsync";
-import sendResponse from "../../../shared/sendResponse";
+import sendResponse, { responseAuthFormat } from "../../../shared/sendResponse";
 import { AuthService } from "./auth.service";
 
 const createUser = catchAsync(async (req: Request, res: Response) => {
@@ -21,11 +21,11 @@ const loginUser = catchAsync(async (req: Request, res: Response) => {
   const userData = req.body;
   const userToken = await AuthService.loginUser(userData);
 
-  sendResponse(res, {
+  responseAuthFormat(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: "User Login successfully!",
-    data: userToken,
+    token: userToken,
   });
 });
 
--- src/shared/sendResponse.ts
+++ src/shared/sendResponse.ts
@@ -12,6 +12,13 @@ type IApiResponse<T> = {
   data?: T | null;
 };
 
+type IAuthData<T> = {
+  statusCode: number;
+  success: boolean;
+  message?: string | null;
+  token?: T | null;
+};
+
 const sendResponse = <T>(res: Response, data: IApiResponse<T>): void => {
   const responseData: IApiResponse<T> = {
     statusCode: data.statusCode,
@@ -25,3 +32,18 @@ const sendResponse = <T>(res: Response, data: IApiResponse<T>): void => {
 };
 
 export default sendResponse;
+
+export const responseAuthFormat = <T>(
+  res: Response,
+  data: IAuthData<T>
+): void => {
+  const responseData: IAuthData<T> = {
+    success: data.success,
+    statusCode: data.statusCode,
+
+    message: data.message || null,
+    token: data.token || null,
+  };
+
+  res.status(data.statusCode).json(responseData);
+};
